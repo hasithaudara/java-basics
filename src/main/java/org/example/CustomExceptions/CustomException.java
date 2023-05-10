119785c15f012ed0b9c4f06e67164472eba50a44
hasithaudara <hasitha.u.bombuwala@gmail.com>
2023-05-10 16:50:45 +0530

    amended custom exceptions class

--- src/main/java/org/example/CustomExceptions/CustomException.java
+++ src/main/java/org/example/CustomExceptions/CustomException.java
@@ -1,8 +1,6 @@
 package org.example.CustomExceptions;
 
-import java.io.IOException;
-
-public class CustomException extends IOException {
+public class CustomException extends Exception {
 
     public String getCustomException() {
         return "Error is : " + super.getMessage() + " and it is in the line :" + super.getStackTrace()[0].getLineNumber();
