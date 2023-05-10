19fbc207020fa24c65ee9fc83824c56b30ddba45
hasithaudara <hasitha.u.bombuwala@gmail.com>
2023-05-10 22:56:49 +0530

    altered App.java file

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,11 +5,6 @@ package org.example;
  */
 public class App {
     public static void main(String[] args) {
-
-        String s2 = "Hello";
-        String s3 = new String("Hello");
-
-        System.out.println(s2.equals(s3)); // false, because s2 and s3 refer to different objects in memory, even though they have the same content
-
+        System.out.println();
     }
 }
