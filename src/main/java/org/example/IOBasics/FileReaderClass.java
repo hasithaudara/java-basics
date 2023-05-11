7456d74ac22035195e01b4f3a45f00ec86af0d7e
hasithaudara <hasitha.u.bombuwala@gmail.com>
2023-05-11 10:43:18 +0530

    altered FileReaderClass to read and write in the same loop

--- src/main/java/org/example/IOBasics/FileReaderClass.java
+++ src/main/java/org/example/IOBasics/FileReaderClass.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class FileReaderClass {
     private final String INPUT_FILE_PATH = "C:\\Users\\HasithaBombuwala\\OneDrive - ConceptVines\\Documents\\Lentra\\java-basics\\src\\main\\java\\org\\example\\Items.csv";
@@ -16,11 +14,11 @@ public class FileReaderClass {
 
         try (BufferedReader fileReader = new BufferedReader(new FileReader(exampleClass.INPUT_FILE_PATH));
              FileWriter outputFileWriter = new FileWriter(exampleClass.OUTPUT_FILE_PATH);) {
+
             int total = 0;
             String line = null;
             while ((line = fileReader.readLine()) != null) {
-                String lineValue = (line != null) ? line.split(",")[1] : "0";
-                total += Integer.parseInt(lineValue);
+                total += Integer.parseInt(line.split(",")[1]);
                 outputFileWriter.write(line.trim() + System.lineSeparator());
             }
             outputFileWriter.write(System.lineSeparator());
